b21fad61cefedc74ae5f80e539522be623b77fb4
Dad <Dad>
2017-01-20 14:43:03 -0500

    T

--- src/org/worker/impl/FillJugs.java
+++ src/org/worker/impl/FillJugs.java
@@ -1,13 +1,16 @@
 package org.worker.impl;
 
 import org.OrionJugs;
+import org.osbot.rs07.api.model.RS2Object;
 import org.worker.OJWorker;
 
 import viking.api.Timing;
 
 public class FillJugs extends OJWorker
 {
-	private long ANIM_THRESH = 600;
+	private static final int WATER_DIST_THRESH = 7; 
+	private static final long ANIM_THRESH = 600;
+	
 	private long lastAnim;
 	
 	public FillJugs(OrionJugs mission)
@@ -23,8 +26,15 @@ public class FillJugs extends OJWorker
 			bank.close();
 		else if(myPlayer().isAnimating())
 			lastAnim = Timing.currentMs();
-		else if(Timing.timeFromMark(lastAnim) > ANIM_THRESH && iFact.itemOnObj("Jug", mission.MANAGER.location.WATER_SOURCE, 20).execute())
-			Timing.waitCondition(() -> myPlayer().isAnimating(), 6500);
+		else if(Timing.timeFromMark(lastAnim) > ANIM_THRESH)
+		{
+			String source = mission.MANAGER.location.WATER_SOURCE;
+			RS2Object water = objects.closest(source);
+			if(water != null && myPosition().distance(water.getPosition()) > WATER_DIST_THRESH)
+				walkUtils.walkPath(mission.MANAGER.location.BANK_PATH);
+			else if(iFact.itemOnObj("Jug", source, 10).execute())
+				Timing.waitCondition(() -> myPlayer().isAnimating(), 6500);
+		}
 	}
 
 }
