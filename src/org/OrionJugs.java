e4019e16cc65e7eb1af09c60d2c268d684418689
Dad <Dad>
2017-01-20 15:25:39 -0500

    T

--- src/org/OrionJugs.java
+++ src/org/OrionJugs.java
@@ -17,7 +17,7 @@ import viking.framework.script.VikingScript;
 
 public class OrionJugs extends Mission implements CapitalMission, ItemManagement, MuleManagement
 {	
-	private static final int JUG_PURCHASE_AMT = 300;
+	private static final int JUG_PURCHASE_AMT = 2000;
 	
 	public final OJ_WorkerManager MANAGER;
 	
