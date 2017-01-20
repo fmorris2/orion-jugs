d6beedaf191f305227ae2eed4a64c38e37e797f6
Dad <Dad>
2017-01-20 14:08:21 -0500

    T

--- src/org/worker/impl/FillJugs.java
+++ src/org/worker/impl/FillJugs.java
@@ -3,9 +3,13 @@ package org.worker.impl;
 import org.OrionJugs;
 import org.worker.OJWorker;
 
+import viking.api.Timing;
+
 public class FillJugs extends OJWorker
 {
-
+	private long ANIM_THRESH = 600;
+	private long lastAnim;
+	
 	public FillJugs(OrionJugs mission)
 	{
 		super(mission);
@@ -15,6 +19,10 @@ public class FillJugs extends OJWorker
 	public void work()
 	{
 		script.log(this, false, "Fill Jugs");
+		if(myPlayer().isAnimating())
+			lastAnim = Timing.currentMs();
+		else if(Timing.timeFromMark(lastAnim) > ANIM_THRESH && iFact.itemOnObj("Jug", "Fountain", 20).execute())
+			Timing.waitCondition(() -> myPlayer().isAnimating(), 6500);
 	}
 
 }
